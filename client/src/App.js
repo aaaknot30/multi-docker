326b76e8f96c5ad4222c30256a935ae2fa78a840
Kyle Larson <aaaknot30@yahoo.com>
2023-12-29 11:44:37 -0700

    update for github3

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React and Caged Tigers!
+            Learn React and Banana Nut!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
