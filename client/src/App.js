028b4cf7dc2fdad44331f04bc490fa79a298ebf0
Kyle Larson <aaaknot30@yahoo.com>
2023-12-29 05:46:30 -0700

    update for github

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React and Glamping Skills!
+            Learn React and Rhino!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
